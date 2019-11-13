1e3e10d4c0c7a2ae96d5242d7ef7d335958680f7
Ayriel Ash <aya37278@bethel.edu>
2019-11-13 09:54:36 -0600

    base

--- server/routes/secrets.router.js
+++ server/routes/secrets.router.js
@@ -2,9 +2,20 @@ const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
 
-router.get('/', (req, res) => {
+
+const { rejectUnauthenticated } = require('../modules/authentication-middleware');
+
+
+// Handles Ajax request for user information if user is authenticated
+/* router.get('/', rejectUnauthenticated, (req, res) => {
+  // Send back user object from the session (previously queried from the database)
+  res.send(req.user);
+}); */
+
+router.get('/', rejectUnauthenticated, (req, res) => {
     console.log('req.user:', req.user);
-    pool.query('SELECT * FROM "secret";')
+    const queryValues=[req.user.clearance_level];
+    pool.query('SELECT * FROM "secret" WHERE secrecy_level< $1;',queryValues)
         .then(results => res.send(results.rows))
         .catch(error => {
             console.log('Error making SELECT for secrets:', error);
